af890160cccfe93ad0ea43270173787c8e231216
sora-00 <sora.umiushi@gmail.com>
2024-05-10 19:25:40 +0900

    buttonの色を変えた

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Box, Button, Typography } from "@mui/material";
 export default function Home() {
   return (
     <Box>
-      <Button variant="contained" href="/sora">
+      <Button variant="contained" color="inherit" href="/sora">
         Contained
       </Button>
       <Typography>muiの勉強会</Typography>
