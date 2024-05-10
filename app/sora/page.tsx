d6002f134a4dac1a412c8b72665dae766a766846
sora-00 <sora.umiushi@gmail.com>
2024-05-10 19:41:05 +0900

    cssを書いてみた

--- app/sora/page.tsx
+++ app/sora/page.tsx
@@ -4,7 +4,20 @@ import React from "react";
 const page = () => {
   return (
     <Box>
-      <Typography>Hello World!</Typography>
+      <Typography
+        fontSize={"100px"}
+        color="red"
+        bgcolor={"green"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        // marginTop={"100px"}  省略するかどうかは一貫性を大事に
+        mt={"100px"}
+        p={3}
+        // ↑数字だけで書くと、これ×８が""で書いたのになる（padding）
+      >
+        Hello World!
+      </Typography>
     </Box>
   );
 };
